852639ccaf47cf5be0e265b7039e2d2251c004a1
Itay Iluz <itay@xsites.co.il>
2020-08-11 11:36:52 +0300

    Remove wrapping function

--- src/tin-validator.js
+++ src/tin-validator.js
@@ -1,42 +1,35 @@
 const { countriesData } = require("./countries-data.js");
 
-const tinValidator = (function() {
+const validateCountryTin = (dataToValidate, countryIso) => {
+  const countryData = countriesData[countryIso];
 
-  let publicMethods = {};
+  if (countryData) {
+    let isTinValid = false;
 
-  publicMethods.validateCountryTin = (dataToValidate, countryIso) => {
-    const countryData = countriesData[countryIso];
+    countryData.tinRules.forEach(tinRule => {
+      if (!isTinValid) {
+        let areAllRuleFunctionsValid = true;
 
-    if (countryData) {
-      let isTinValid = false;
-
-      countryData.tinRules.forEach(tinRule => {
-        if (!isTinValid) {
-          let areAllRuleFunctionsValid = true;
-
-          tinRule.forEach(ruleFunction => {
-            if (areAllRuleFunctionsValid) {
-              try {
-                areAllRuleFunctionsValid = ruleFunction.call(
-                  this,
-                  dataToValidate
-                );
-              } catch (e) {
-                areAllRuleFunctionsValid = false;
-              }
+        tinRule.forEach(ruleFunction => {
+          if (areAllRuleFunctionsValid) {
+            try {
+              areAllRuleFunctionsValid = ruleFunction.call(
+                this,
+                dataToValidate
+              );
+            } catch (e) {
+              areAllRuleFunctionsValid = false;
             }
-          });
-          isTinValid = areAllRuleFunctionsValid;
-        }
-      });
+          }
+        });
+        isTinValid = areAllRuleFunctionsValid;
+      }
+    });
 
-      return isTinValid;
-    } else {
-      throw "Invalid country ISO";
-    }
-  };
+    return isTinValid;
+  } else {
+    throw "Invalid country ISO";
+  }
+};
 
-  return publicMethods;
-})();
-
-module.exports = { tinValidator };
+module.exports = { validateCountryTin };
