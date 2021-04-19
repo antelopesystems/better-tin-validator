f7357f712b1e9e2ec06cd999a5be61f6db51620e
Maisam Mansour <maisamm@xsites.co.il>
2021-04-19 13:50:43 +0300

    Fix index.js module exports

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var validateCountryTin = require("./build/tin-validator").validateCountryTin;
 var getTinErrorMessage = require("./build/getTinErrorMessage").getTinErrorMessage;
 
-module.exports = {
-    validateCountryTin,
-    getTinErrorMessage
-}
+module.exports.validateCountryTin = validateCountryTin
+module.exports.getTinErrorMessage = getTinErrorMessage
+
